72d82423f444606e4e420ccdb8e00d537262c30d
yashu3399 <yashas3399@gmail.com>
2026-04-28 12:51:06 +0530

    updated app.js

--- hospital-managemant/src/App.js
+++ hospital-managemant/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import AdminPanel from "./pages/AdminPanel";
+import PatientRegistration from "./pages/PatientRegistration";
+import Appointment from "./pages/Appointment";
+import DoctorList from "./pages/DoctorList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/admin" element={<AdminPanel />} />
+
+        <Route path="/patients" element={<PatientRegistration />} />
+        <Route path="/appointments" element={<Appointment />} />
+        <Route path="/doctors" element={<DoctorList />} />
+      </Routes>
+    </Router>
   );
 }
 
